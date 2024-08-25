fc75c5ac7adef6f56bc08f94e4127d3c98ecfd8a
Maher Alkahfi Yalova <andremuchlisin98@gmail.com>
2024-08-25 21:41:51 +0700

    new

--- main.py
+++ main.py
@@ -4,10 +4,20 @@ Ini adalah demo
 
 
 # percabangan
-jumlah_botol_susu = 0
+jumlah_botol_susu = 173
+jumlah_telur = 1000
+print(f"jumlah botol susu {jumlah_botol_susu} botol")
+print(f"jumlah telur {jumlah_telur} telur")
 
 if jumlah_botol_susu > 0:
-    print("my name Andre Muchlisin")
+    print("Budi mengecek harganya, ternyata cukup")
 
+    if jumlah_telur == 0:
+        print("budi membeli 1 botol susu")
+    else:
+        print("Budi membeli 6 botol susu")
 else:
-    print("my name kagiru")
+     print("budi tidak jadi membeli")
+
+ print("budi pulang kerumah")
+print("budi menyampaikan ke ibunya")
