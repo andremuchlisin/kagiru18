3f9533aaaa30bd18cb05551c83604636ccd43040
Maher Alkahfi Yalova <andremuchlisin98@gmail.com>
2024-08-28 21:04:27 +0700

    perulangan dengan while dengan jumlah tak terbatas dan ada variable

--- perulangan dengan gabungan.py
+++ perulangan dengan gabungan.py
@@ -8,7 +8,7 @@ read_count = 0
 understood_count = 0
 print(f'jumlah buku yang sudah dibaca dan dipahami {understood_count}')
 
-while read_count < bbok_count * 2:
+while read_count < book_count * 2:
     read_count = read_count + 1
     if understood_count == 9:
         print(f'buku ke {understood_count + 1} belum paham')
@@ -17,7 +17,7 @@ while read_count < bbok_count * 2:
         print(f'buku ke {understood_count} sudah dibaca dan dipahami')
 
 print(f'Jumlah buku yang sudah dibaca {understood_count}')
-if understood_count == bbok_count:
+if understood_count == book_count:
     print(f'Bu, semua buku sudah dibaca dan dipahami')
 else:
     print(f'Bu, tidak semua buku bis diphami. '
